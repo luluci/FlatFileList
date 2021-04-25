9d51c86ccfc7df26855c68157a52d35b36dc9141
luluci <lunacied@gmail.com>
2021-04-25 12:02:53 +0900

    add file change event handler

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {TreeItem, flatfilelistView} from './flatfilelistView';
+import {TreeItem, FlatFileListView} from './flatfilelistView';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -25,7 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 	// FlatFileListView 定義
-	const vscl = new flatfilelistView(context);
+	const vscl = new FlatFileListView(context);
 	vscode.window.registerTreeDataProvider<TreeItem>('flatfilelistView', vscl);
 }
 
--- src/flatfilelistView.ts
+++ src/flatfilelistView.ts
@@ -1,49 +1,183 @@
+
 import * as vscode from 'vscode';
 import {posix} from 'path';
 
 export class TreeItem extends vscode.TreeItem {
-	constructor(dir: string, file_uri :vscode.Uri) {
+	constructor(dir: string, fileUri :vscode.Uri) {
 		// 基底クラスコンストラクタ
-		super(file_uri, vscode.TreeItemCollapsibleState.None);
+		// resourceUriで初期化
+		// labelはresourceUrから設定される
+		super(fileUri, vscode.TreeItemCollapsibleState.None);
 		// 拡張情報
+		if (dir === "") {
+			dir = "/";
+		}
 		this.description = dir;
 		this.command = {
 			title: "open file",
 			command: "vscode.open",
-			arguments: [file_uri]
+			arguments: [fileUri]
 		};
 	}
 }
 
-export class flatfilelistView implements vscode.TreeDataProvider<TreeItem> {
+export class FlatFileListView implements vscode.TreeDataProvider<TreeItem> {
 	private filetree: TreeItem[];
-
+	private _onDidChangeTreeData: vscode.EventEmitter<TreeItem | null> = new vscode.EventEmitter<TreeItem | null>();
+	readonly onDidChangeTreeData: vscode.Event<TreeItem | null> = this._onDidChangeTreeData.event;
+	private _fileSystemWatcher: Array<vscode.FileSystemWatcher>;
+	
 	constructor(private context: vscode.ExtensionContext) {
-		const root_dirs = vscode.workspace.workspaceFolders;
+		// イベント登録
+		vscode.workspace.onDidChangeWorkspaceFolders(e => this.onWorkspaceChanged(e));
+		// workspace内のファイル追加,削除,リネームを検出する
+		// VSCode外からの操作も検出するためにFileSystemWatcherを使用する
+		//vscode.workspace.onDidCreateFiles(e => this.onFileCreated(e));
+		//vscode.workspace.onDidDeleteFiles(e => this.onFileDeleted(e));
+		//vscode.workspace.onDidRenameFiles(e => this.onFileRenamed(e));
+		//
+		this._fileSystemWatcher = [];
+		const rootDirs = vscode.workspace.workspaceFolders;
+		if (rootDirs) {
+			for (let i = 0; i < rootDirs.length; i++) {
+				let fileWatcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(rootDirs[i], "**/*.*"));
+				fileWatcher.onDidCreate(e => this.onFWCreated(e));
+				fileWatcher.onDidDelete(e => this.onFWDeleted(e));
+				fileWatcher.onDidChange(e => this.onFWChanged(e));
+				this._fileSystemWatcher.push(fileWatcher);
+			}
+		}
+		// Treeデータ初期化
 		this.filetree = [];
+		
+	}
+
+	refresh(item: TreeItem|null): void {
+		this._onDidChangeTreeData.fire(item);
+	}
+
+	sort() {
+		
+	}
+
+	private onFWCreated(uri: vscode.Uri) {
+		console.log(":onFWCreated:");
+		console.log(uri);
+		// 新規ファイル
+		let relPath = this.makeRelPath(uri);
+		let item = new TreeItem(relPath, uri);
+		// Treeに追加
+		this.filetree.push(item);
+		// refresh
+		this.refresh(null);
+	}
+	private onFWDeleted(uri: vscode.Uri) {
+		console.log(":onFWDeleted:");
+		console.log(uri);
+		// 要素削除
+		this.filetree = this.filetree.filter((item) => {
+			if (item.resourceUri) {
+				return uri.path !== item.resourceUri.path;
+			} else {
+				return false;
+			}
+		});
+		this.refresh(null);
+	}
+	private onFWChanged(event: vscode.Uri) {
+		// ファイル内容変更時イベント
+		//console.log(":onFWChanged:");
+		//console.log(event);
+	}
+
+	private onWorkspaceChanged(event: vscode.WorkspaceFoldersChangeEvent): void {
+		console.log(event);
+	}
+	private onFileCreated(event: vscode.FileCreateEvent) {
+		console.log(event);
+		for (let i=0; i<event.files.length; i++) {
+			// 新規ファイル
+			let newUri = event.files[i];
+			let relPath = this.makeRelPath(newUri);
+			let item = new TreeItem(relPath, newUri);
+			// Treeに追加
+			this.filetree.push(item);
+		}
+		this.refresh(null);
+	}
+	private onFileDeleted(event: vscode.FileDeleteEvent) {
+		console.log(event);
+		// 削除ファイルリスト作成
+		let delUriList = Array<string>();
+		for (let i = 0; i < event.files.length; i++) {
+			delUriList.push(event.files[i].path);
+		}
+		// 要素削除
+		this.filetree = this.filetree.filter((item)=>{
+			if (item.resourceUri) {
+				return !delUriList.includes(item.resourceUri.path);
+			} else {
+				return false;
+			}
+		});
+		this.refresh(null);
+	}
+	private onFileRenamed(event: vscode.FileRenameEvent) {
+		console.log(event);
+		let tgtItem: TreeItem|undefined = undefined;
+		for (let i=0; i<event.files.length; i++) {
+			let oldUri = event.files[i].oldUri;
+			let newUri = event.files[i].newUri;
+			// TreeItem更新
+			let item = this.filetree.find(item => item.resourceUri?.path === oldUri.path);
+			if (item) {
+				item.resourceUri = newUri;
+				tgtItem = item;
+			}
+		}
+		if (tgtItem) {
+			//
+			this.refresh(tgtItem);
+		}
+	}
+
+	private makeRelPath(fileUri: vscode.Uri) {
+		const rootDirs = vscode.workspace.workspaceFolders;
+		if (rootDirs) {
+			for (let i = 0; i < rootDirs.length; i++) {
+				let result = posix.relative(rootDirs[i].uri.path, fileUri.path);
+				let relDir = posix.dirname(result);
+				if (relDir === ".") {
+					return "";
+				} else if (relDir !== "") {
+					return "/" + relDir;
+				}
+			}
+		}
+		return "";
 	}
 
 	async generateTree() {
-		const root_dirs = vscode.workspace.workspaceFolders;
-		if (root_dirs !== undefined) {
+		const rootDirs = vscode.workspace.workspaceFolders;
+		if (rootDirs !== undefined) {
 			// ディレクトリリスト作成
-			let dir_list: Array<[vscode.Uri, string]> = [];
-			for (let i = 0; i < root_dirs.length; i++) {
-				dir_list.push([root_dirs[i].uri, ""])
+			let dirList: Array<[vscode.Uri, string]> = [];
+			for (let i = 0; i < rootDirs.length; i++) {
+				dirList.push([rootDirs[i].uri, ""]);
 			}
 			// ディレクトリ探索
-			for (let i = 0; i < dir_list.length; i++) {
-				const [dir_uri, rel_path] = dir_list[i];
-				for (const [name, type] of await vscode.workspace.fs.readDirectory(dir_uri)) {
+			for (let i = 0; i < dirList.length; i++) {
+				const [dirUri, relPath] = dirList[i];
+				for (const [name, type] of await vscode.workspace.fs.readDirectory(dirUri)) {
 					if (type === vscode.FileType.File) {
-						let file_uri = vscode.Uri.parse(posix.join(dir_uri.path, name));
-						let item = new TreeItem(rel_path, file_uri);
+						let fileUri = vscode.Uri.parse(posix.join(dirUri.path, name));
+						let item = new TreeItem(relPath, fileUri);
 						this.filetree.push(item);
 						console.log(name);
 					} else if (type === vscode.FileType.Directory) {
 						// 子ディレクトリURIを作成
-						let child = vscode.Uri.parse(posix.join(dir_uri.path, name));
-						dir_list.push([child, rel_path + "/" + name]);
+						let child = vscode.Uri.parse(posix.join(dirUri.path, name));
+						dirList.push([child, relPath + "/" + name]);
 					}
 				}
 			}
@@ -56,7 +190,7 @@ export class flatfilelistView implements vscode.TreeDataProvider<TreeItem> {
 	}
 
 	getChildren(element?: TreeItem): Thenable<TreeItem[]> {
-		if (element === undefined) {
+		if (this.filetree.length === 0) {
 			return Promise.resolve(this.generateTree());
 		} else {
 			return Promise.resolve(this.filetree);
